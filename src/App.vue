548bd98d2055fa8a790d890c0abb79a7c03b78e1
yuoper1 <yuoper1@gmail.com>
2024-04-13 11:51:46 +0900

    vuetify 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,35 @@
 <template>
   <v-app>
-asdwf
+    <v-layout>
+      <v-navigation-drawer
+          expand-on-hover
+          rail
+      >
+        <v-list>
+          <v-list-item
+              prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+              subtitle="sandra_a88@gmailcom"
+              title="Sandra Adams"
+          >
+            vuetify
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles">
+            <router-link to="/"> home </router-link>
+          </v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared">
+            <router-link to="/grid-system">GridSystem</router-link>
+          </v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 250px">    <router-view /></v-main>
+    </v-layout>
   </v-app>
 </template>
 
@@ -14,3 +43,4 @@ export default {
   }),
 };
 </script>
+`
